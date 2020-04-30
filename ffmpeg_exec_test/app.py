57ad9faa1d715a41ef25e2411b9ddd5ecf7accb7
mission_code_now <nonamenomail@gmail.com>
2020-04-30 18:18:37 +0300

    touch on ffmpeg

---
+++ ffmpeg_exec_test/app.py
@@ -0,0 +1,42 @@
+import cv2
+import time
+import ffmpeg_streaming
+from ffmpeg_streaming import Formats, Bitrate, Representation, Size
+
+from flask import render_template, Flask, send_from_directory, abort, json, \
+   Response  # sudo python3 -m pip install flask
+
+app = Flask('__name__')
+
+@app.route('/')
+def index():
+   """Video streaming home page."""
+   return render_template('index.html')
+
+def gen():
+   """Video streaming generator function."""
+   x = 0
+   while x < 7:
+      cap = cv2.VideoCapture('/var/media/_480p/hls_outputs_480p_000' + str(x) + '.ts')
+      x = x + 1
+
+      # Read until video is completed
+      while (cap.isOpened()):
+         # Capture frame-by-frame
+         ret, img = cap.read()
+         if ret == True:
+            img = cv2.resize(img, (0,0), fx=1.4, fy=1.1)
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+         else:
+            break
+
+@app.route('/video_feed')
+def video_feed():
+   """Video streaming route. Put this in the src attribute of an img tag."""
+   return Response(gen(),
+                   mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+   app.run(host = "0.0.0.0", port = '5000')
---
+++ ffmpeg_exec_test/capture.py
@@ -0,0 +1,20 @@
+import ffmpeg_streaming
+from ffmpeg_streaming import Formats, Bitrate, Representation, Size
+
+import sys
+
+def monitor(ffmpeg, duration, time_):
+    per = round(time_ / duration * 100)
+    sys.stdout.write("\rTranscoding...(%s%%) [%s%s]" % (per, '#' * per, '-' * (100 - per)))
+    sys.stdout.flush()
+
+video = ffmpeg_streaming.input('http://wmccpinetop.axiscam.net/mjpg/video.mjpg')
+
+_360p = Representation(Size(640, 360), Bitrate(276 * 1024, 128 * 1024))
+_480p = Representation(Size(854, 480), Bitrate(750 * 1024, 192 * 1024))
+_720p = Representation(Size(1280, 720), Bitrate(2048 * 1024, 320 * 1024))
+
+hls_stream = video.hls(Formats.h264())
+hls_stream.flags('delete_segments');
+hls_stream.representations(_480p)
+hls_stream.output('/var/media/_480p/hls_outputs.m3u8')
